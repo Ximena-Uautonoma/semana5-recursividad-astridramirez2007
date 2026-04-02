c79790168b567adccf6d4a490df762d5313d5dce
astridramirez2007 <astrid.ramirez@cloud.uautonoma.cl>
2026-04-02 14:40:54 +0000

    Hii

--- Semana5/ejercicio2.py
+++ Semana5/ejercicio2.py
@@ -11,11 +11,21 @@ def suma_ciclo(n):
     """
     Retorna la suma de los primeros n números usando un ciclo.
     """
-    pass
+    total=0
+    for i in range(1, n+1):
+        total+= i
+    return total
+print(suma_ciclo(4))
+
 
 
 def suma_recursiva(n):
     """
     Retorna la suma de los primeros n números usando recursividad.
     """
-    pass
+    if n==1:
+        return 1
+    return n+ suma_recursiva(n-1)
+print(suma_recursiva(4))
+
+
